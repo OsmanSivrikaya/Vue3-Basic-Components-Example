0467b4752c9a812ab418297bb91355ff21101f19
OsmanSivrikaya <95168500+OsmanSivrikaya@users.noreply.github.com>
2024-02-23 08:25:46 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <app-header></app-header>
+  <div class="container">
+    <h3> Deneme </h3>
+    <p>asdasd</p>
+  </div>
+  <counterItem></CounterItem>
+</template>
+<script>
+  import CounterItem from "@/components/CounterItem.vue";
+  export default{
+    components: {
+      CounterItem : CounterItem,
+    },
+  }
+</script>
